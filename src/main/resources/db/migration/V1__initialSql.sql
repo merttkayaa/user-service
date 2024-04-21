dc7874803074338857e845985567ed9fc2e93ee9
mert <mk754@sussex.ac.uk>
2024-04-21 21:40:24 +0100

    bug fix

--- src/main/resources/db/migration/V1__initialSql.sql
+++ src/main/resources/db/migration/V1__initialSql.sql
@@ -3,12 +3,12 @@ CREATE SCHEMA users;
 create table users.users
 (
     id                  bigint PRIMARY KEY,
-    firstName             varchar(10)      not null,
-    lastName              varchar(10)      not null,
-    cardNumber            varchar(10)      not null,
+    first_name             varchar(10)      not null,
+    last_name              varchar(10)      not null,
+    card_number            varchar(10)      not null,
     cvv                   varchar(10)      not null,
-    expireMonth           varchar(10)      not null,
-    expireYear            varchar(10)      not null
+    expire_month           varchar(10)      not null,
+    expire_year            varchar(10)      not null
 );
 
 create sequence users.users_sequence
